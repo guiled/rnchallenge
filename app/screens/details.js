8b8931c4b4e30fa43fb16454d60a76168d86d1af
Guislain Duthieuw <gduthieuw@houra.fr>
2021-01-30 22:19:32 +0100

    Display restaurant details

--- app/screens/details.js
+++ app/screens/details.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import { View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+import DText from '../components/Text';
 
 const Detail = ({ route, navigation }) => {
-    return <View />;
+    return <View style={styles.detailContainer}>
+        <DText style={styles.title}>{route.params.item.name}</DText>
+        <Image
+            style={styles.headImage}
+            source={{ uri: route.params.item.heroMedia.url }}
+        />
+        <DText style={styles.description}>{route.params.item.shortDescription}</DText>
+    </View>;
 };
+
+const styles = StyleSheet.create({
+    detailContainer: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignContent: "flex-start",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: "center"
+    },
+    headImage: {
+        marginTop: 10,
+        width: undefined,
+        height: 230
+    },
+    description: {
+        fontSize: 16,
+        margin: 20,
+        textAlign: "justify",
+        lineHeight: 25
+    }
+})
 
 export default Detail;
